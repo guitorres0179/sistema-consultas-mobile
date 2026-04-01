43700b0f272c5b9daa0b3285265c4774f9920186
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-31 22:35:20 -0300

    Aguardando...

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+          <Button title="Confirmar Consulta" onPress={confirmarConsulta} />
+        )}
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  card: {
+    width: "80%",
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 8,
   },
 });
